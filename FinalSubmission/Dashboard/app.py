adf4edf64610d3edeeca016f2a21b0626818f56a
Jessica O <81931427+alojessica@users.noreply.github.com>
2025-12-12 01:09:48 -0500

    Deploy mental health dashboard to Render

--- FinalSubmission/Dashboard/app.py
+++ FinalSubmission/Dashboard/app.py
@@ -761,6 +761,7 @@ external_stylesheets = [dbc.themes.BOOTSTRAP]
 app = Dash(
     __name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True
 )
+server = app.server
 
 app.layout = html.Div(
     [
